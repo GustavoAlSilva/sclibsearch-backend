4b171a337353d8e3b76ee110ee922f648725373d
Gustavo Silva <gustavo.silva@sofit4.com.br>
2024-06-08 20:46:54 -0300

    feature(user.controller): implementing user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,37 +6,43 @@ import {
     Patch,
     Param,
     Delete,
+    HttpStatus,
+    HttpCode,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+@Controller('api/v1/user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Post()
-    create(@Body() createUserDto: CreateUserDto) {
+    async create(@Body() createUserDto: CreateUserDto) {
         return this.userService.create(createUserDto);
     }
 
     @Get()
-    findAll() {
+    async findAll() {
         return this.userService.findAll();
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.userService.findOne(+id);
+    async findOne(@Param('id') id: number) {
+        return this.userService.findOne(id);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return this.userService.update(+id, updateUserDto);
+    async update(
+        @Param('id') id: number,
+        @Body() updateUserDto: UpdateUserDto,
+    ) {
+        return this.userService.update(id, updateUserDto);
     }
 
     @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.userService.remove(+id);
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async remove(@Param('id') id: number) {
+        await this.userService.remove(id);
     }
 }
