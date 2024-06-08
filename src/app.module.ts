f21738609ed7d2409d8dd9be03a156a118f7decc
Gustavo Silva <gustavo.silva@sofit4.com.br>
2024-06-08 17:24:08 -0300

    feature(app.module): creating connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [UserModule],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: process.env.TYPEORM_TYPE,
+            host: process.env.TYPEORM_HOST,
+            port: process.env.TYPEORM_PORT,
+            username: process.env.TYPEORM_USERNAME,
+            password: process.env.TYPEORM_PASSWORD,
+            database: process.env.TYPEORM_DATABASE,
+            entities: [__dirname + '/**/*.entity{.js,.ts}'],
+            synchronize: true,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        } as TypeOrmModuleOptions),
+        UserModule,
+    ],
     controllers: [],
     providers: [],
 })
