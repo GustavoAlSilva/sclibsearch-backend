2be6a8bfe408c4927c4f071b6bbb5847189ceee2
Gustavo Silva <gustavo.silva@sofit4.com.br>
2024-06-08 20:43:35 -0300

    feature(messages.helper): adding `USER_NOT_FOUND` message

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -2,4 +2,5 @@ export const MessagesHelper = {
     PASSWORD_VALID:
         'A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
     PASSWORD_OR_EMAIL_INVALID: 'E-mail e/ou senha são inválidos',
+    USER_NOT_FOUND: 'Usuário não encontrado',
 };
