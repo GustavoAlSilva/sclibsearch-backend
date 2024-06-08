cb408ea135e3e243d16502844c28b889f33b54fe
Gustavo Silva <gustavo.silva@sofit4.com.br>
2024-06-08 19:32:15 -0300

    feature(user): implementing user entity & user dtos

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsPhoneNumber, Matches } from 'class-validator';
+import { MessagesHelper } from '../../helpers/messages.helper';
+import { RegExHelper } from '../../helpers/regex.helper';
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @Matches(RegExHelper.password, { message: MessagesHelper.PASSWORD_VALID })
+    password: string;
+
+    @IsPhoneNumber('BR')
+    phone: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,41 @@
-export class User {}
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ name: 'first_name' })
+    firstName: string;
+
+    @Column({ name: 'last_name' })
+    lastName: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    phone: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: string;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: string;
+
+    @DeleteDateColumn({ name: 'deleted_at' })
+    deletedAt: string;
+}
