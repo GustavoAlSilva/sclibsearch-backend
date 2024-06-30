99b2ec73f4bc8ffd5fd394f4d6c87aa166763d02
Gustavo Silva <gustavo.silva@sofit4.com.br>
2024-06-30 16:36:50 -0300

    feature(messages.helper): adding `RENTAL_NOT_FOUND` message

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -4,4 +4,5 @@ export const MessagesHelper = {
     PASSWORD_OR_EMAIL_INVALID: 'E-mail e/ou senha são inválidos',
     USER_NOT_FOUND: 'Usuário não encontrado',
     BOOK_NOT_FOUND: 'Livro não encontrado',
+    RENTAL_NOT_FOUND: 'Locação não encontrada',
 };
