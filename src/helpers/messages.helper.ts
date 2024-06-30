fafd978cf01568d63438ca05ed71154eeb3d4264
Gustavo Silva <gustavo.silva@sofit4.com.br>
2024-06-30 14:44:57 -0300

    feature(messages.helper): adding `BOOK_NOT_FOUND` message

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -3,4 +3,5 @@ export const MessagesHelper = {
         'A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
     PASSWORD_OR_EMAIL_INVALID: 'E-mail e/ou senha são inválidos',
     USER_NOT_FOUND: 'Usuário não encontrado',
+    BOOK_NOT_FOUND: 'Livro não encontrado',
 };
