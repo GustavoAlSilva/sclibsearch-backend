c2552789aed25826b5d25a1daebe936a77d553c8
Gustavo Silva <gustavo.silva@sofit4.com.br>
2024-06-30 15:11:01 -0300

    feature(book.module): injecting the BookEntity

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookService } from './book.service';
 import { BookController } from './book.controller';
+import { BookEntity } from './entities/book.entity';
 
 @Module({
-  controllers: [BookController],
-  providers: [BookService],
+    imports: [TypeOrmModule.forFeature([BookEntity])],
+    controllers: [BookController],
+    providers: [BookService],
 })
 export class BookModule {}
